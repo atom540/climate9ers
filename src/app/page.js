0938456e767f3b8a9ab9731330546844ffef60c0
Aniyeshu-31 <aniyeshu31@gmail.com>
2024-02-16 19:28:15 +0530

    updated!!

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-import Navbar from "@/components/navbar/Navbar"
-const page = () => {
-  return (
-    <div>
-      <Navbar />
-=======
 import Team from '@/components/team/Team'
-import React from 'react'
+import Navbar from '@/components/navbar/Navbar'
 
 const page = () => {
   return (
     <div>
-      <Team/>
->>>>>>> 2d66fcff9a98fd0bd8f66dfe692eb780f59ed074
+      <Navbar />
+      <Team />
     </div>
   )
 }
